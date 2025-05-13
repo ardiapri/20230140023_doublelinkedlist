1069ee4bc840eb978dec30b9112355d5c496277f
ardiapri <ardiapri882@gmail.com>
2025-05-13 11:20:51 +0700

    prosedur traverse untuk menampilkan data secara urut

--- doubelinkedlist.cpp
+++ doubelinkedlist.cpp
@@ -146,3 +146,20 @@ bool listEmpty()
 {
     return (START == NULL);
 }
+// prosedur traverser untuk menampilkan data secara urut 
+void traverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in ascending order of roll number are: " << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
