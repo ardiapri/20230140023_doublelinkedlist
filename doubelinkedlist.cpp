6588e317de94b0f6182f1d07c16d34aa8c2bd473
ardiapri <ardiapri882@gmail.com>
2025-05-13 10:30:53 +0700

    pemuatan node dan pemberian value untuk data noMhs dan name

--- doubelinkedlist.cpp
+++ doubelinkedlist.cpp
@@ -14,3 +14,12 @@ struct Node
 //deklarasi pointer start dan pemberian nilai
 Node* START = NULL;
 void addNode()
+{
+    //pemuatan node dan pemberian value untuk data noMhs dan name
+    Node* newNode = new Node(); //step1 buat Node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; //assign value to the field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; //asign value to the data field of th
+
+    
