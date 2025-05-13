98bfc53fb354e47c11783f7eafe3d735fc0e5134
ardiapri <ardiapri882@gmail.com>
2025-05-13 10:27:48 +0700

    deklarasi pointer next dan prev untuk menunjukan ke node berikutnya dan sebelumnya

--- doubelinkedlist.cpp
+++ doubelinkedlist.cpp
@@ -6,4 +6,8 @@ struct Node
     //deskripsi noMhs dan name untuk menampung data
     int noMhs;
     string name;
-    
+    //deskripsi pointer next dan prev untuk menunjukan ke node berikutnya dan sebelumnya
+    Node* next;
+    Node* prev;
+};
+
